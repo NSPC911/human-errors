6e6cc47889ffc4c8a954b5c19dce28083697f108
NSPC911 <87571998+NSPC911@users.noreply.github.com>
2025-11-03 01:34:42 +0800

    yaml: add support for pyyaml
    resolves #2

--- src/human_errors/__init__.py
+++ src/human_errors/__init__.py
@@ -1,5 +1,6 @@
 from .base_renderer import dump
 from .json_renderer import json_dump
 from .toml_renderer import toml_dump
+from .yaml_renderer import yaml_dump
 
-__all__ = ["dump", "json_dump", "toml_dump"]
+__all__ = ["dump", "json_dump", "toml_dump", "yaml_dump"]
